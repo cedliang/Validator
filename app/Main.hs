137e538a5c94692a88a9f1f62dd67a326b9d6b51
cedliang <accounts@cedric.app>
2022-05-29 20:24:25 +1000

    new version in package

--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,7 @@ data TestException = Except0
 
 main :: IO ()
 main = do
-  r <- validate $ ioTrueMonoid <> ioTrueMonoid <> vGroup1 <> vGroup2
+  r <- validate $ veach [(pure True, "123"), (pure False, "456")] <> ioTrue
   case r of
     Success -> print r
     Fail e  -> putStrLn $ "Fail " ++ e
@@ -19,7 +19,7 @@ ioTrueMonoid :: MValidator IO String
 ioTrueMonoid = MVal (print "0" >> pure True) "True Monoid Exception"
 
 ioTrue :: Validator IO String
-ioTrue = Val (print "0" >> pure True) "True Monoid Exception"
+ioTrue = veach [(print "0" >> pure True, "True Monoid Exception")]
 
 ioFalseMonoid :: MValidator IO String
 ioFalseMonoid = MVal (print "1" >> pure False) "False Monoid Exception"
